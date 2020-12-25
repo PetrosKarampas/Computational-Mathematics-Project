8c39c6b3c4615165da3a7ca0da769d16c95d15bc
Alexandros Alexiou <AlexandrosAlexiou@users.noreply.github.com>
2020-12-25 14:11:17 +0200

    save plots to the plots folder

--- Computational Mathematics Project/main.c
+++ Computational Mathematics Project/main.c
@@ -9,14 +9,14 @@
 #include "../include/supportLib.h"
 
 int main(){
-    double x []  = {-5, 0, 1, 2, 3, 4, 5, 6};
+    double x []  = {-10, 0, 1, 2, 3, 4, 5, 6};
     double y []  = {-5, -4, -3, -2, 1, 0, 1, 2};
     RGBABitmapImageReference *imageRef = CreateRGBABitmapImageReference();
     
     DrawScatterPlot(imageRef, 1000, 600, x, 8, y, 8);
     size_t length;
     double *pngData = ConvertToPNG(&length, imageRef->image);
-    WriteToFile(pngData, length, "plot.png");
-    system("open plot.png");
+    WriteToFile(pngData, length, "plots/plot.png");
+    system("open plots/plot.png");
     return 0;
 }
