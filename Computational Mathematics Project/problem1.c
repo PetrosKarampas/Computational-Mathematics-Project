a3bdb554979f701be27e488ab2186b698a54ca31
Alexandros Alexiou <AlexandrosAlexiou@users.noreply.github.com>
2020-12-27 13:53:34 +0200

    Remove pbplot library

--- Computational Mathematics Project/problem1.c
+++ Computational Mathematics Project/problem1.c
@@ -7,8 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "../include/pbPlots.h"
-#include "../include/supportLib.h"
 
 // Constants
 #define M       1
@@ -29,16 +27,15 @@ double Cpsi = 5.0-((double)AM/5000);
 
 //Macros
 #define fx(t, x, y) (y)
-#define fy(t, x, y) ((fz - (g * M) - (Cz * Absolute(y) * y))/M)
+#define fy(t, x, y) ((fz - (g * M) - (Cz * fabs(y) * y))/M)
 #define fw(t, w, u) (u)
-#define fu(t, w, u) ((tz - (0.5 * Cpsi *  Absolute(u) * u))/Iz)
+#define fu(t, w, u) ((tz - (0.5 * Cpsi *  fabs(u) * u))/Iz)
 
 //prtotypes
-void createPlot(double y[], double t[], wchar_t* ylabel, wchar_t* xlabel, wchar_t* graphTitle, char * filename);
+void createPlotData(double y[], double t[], char * filename);
 
 
 int main(){
-    
     double y [30002];
     double y2 [30002];
     double t [30002];
@@ -49,7 +46,7 @@ int main(){
     double y_w_1 = psi0;
     double y_u_1 = psipar0;
     
-    for(int i = 1; i<=30001; i++, time+=h ){
+    for(int i = 1; i<=30001; i++, time+=h ) {
         
         y[i] = y_x_1;
         y_x_1 = y_x_1 + h * fx(time, y[i], y_y_1);
@@ -60,57 +57,23 @@ int main(){
         y_u_1 = y_u_1 + h * fu(time, y2[i], y_u_1);
         
         t[i] = time;
-        
     }
 
 //    int i;
 //    for (i=0; i < (sizeof (y2) /sizeof (y2[0])); i++) {
 //        printf("%lf\n",y2[i]);
 //    }
-   
-    // Plotting stuff
-    y[0]=0;
-    t[0]=0;
-    createPlot(y, t, L"position", L"time", L"Euler's Method", "plot.png");
-    createPlot(y, t, L"look", L"time", L"test", "plot2.png");
-    
-
+    createPlotData(y, t, "euler_method_z.txt");
+    createPlotData(y2, t, "euler_method_y.txt");
     return 0;
 }
 
 
+void createPlotData(double y[], double t[], char * filename) {
+    FILE *fp=NULL;
+    fp=fopen(filename,"w");
 
-void createPlot(double y[], double t[], wchar_t* ylabel, wchar_t* xlabel, wchar_t* graphTitle, char * filename){
-    ScatterPlotSeries *series = GetDefaultScatterPlotSeriesSettings();
-    series->xs = t;
-    series->xsLength = 30002;
-    series->ys = y;
-    series->ysLength = 30002;
-    series->linearInterpolation = true;
-    series->lineTypeLength = wcslen(series->lineType);
-    series->lineThickness = 2;
-    series->color = GetBlack(0.6);
-
-    ScatterPlotSettings *settings = GetDefaultScatterPlotSettings();
-    settings->width = 1000;
-    settings->height = 600;
-    settings->autoBoundaries = true;
-    settings->autoPadding = true;
-    settings->title = graphTitle;
-    settings->titleLength = wcslen(settings->title);
-    settings->xLabel = xlabel;
-    settings->xLabelLength = wcslen(settings->xLabel);
-    settings->yLabel = ylabel;
-    settings->yLabelLength = wcslen(settings->yLabel);
-    ScatterPlotSeries *s [] = {series};
-    settings->scatterPlotSeries = s;
-    settings->scatterPlotSeriesLength = 1;
-
-    RGBABitmapImageReference *canvasReference = CreateRGBABitmapImageReference();
-    DrawScatterPlotFromSettings(canvasReference, settings);
-
-    size_t length;
-    double *pngdata = ConvertToPNG(&length, canvasReference->image);
-    WriteToFile(pngdata, length, filename);
-    DeleteImage(canvasReference->image);
+    for(int i = 1; i<=30001; i++) {
+        fprintf(fp,"%lf\t %lf\n",t[i],y[i]);
+    }
 }
